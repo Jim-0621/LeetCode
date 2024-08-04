046ea95d514d95081baa1dfaf3c86d1c12bb73b2
Jim_0621 <1031352726@qq.com>
2024-08-04 18:08:16 +0800

    feat: review

--- test.java
+++ test.java
@@ -1,18 +1,31 @@
 public class test {
     public static void main(String[] args) {
-//        String s = "sfadf";
-//        System.out.println(s.charAt(2));
-//        String s = "2,3,3";
-//        String[] parts = s.split(",");
-//        int n = (int)(f(32)/f(16));
-//        System.out.println(n);
-        System.out.println(f(10));
+
+        int[][] trips = new int[][]{{2,1,5},{3,3,7}};
+        int capacity = 4;
+        boolean flag = carPooling(trips, capacity);
+
+        System.out.println("Leetcode 每日一题");
     }
-    public static double f(int n){
-        if(n == 1 ){
-            return 1;
-        }else{
-            return f(n-1) *n;
+    public static boolean carPooling(int[][] trips, int capacity) {
+        int far = 0;
+        int len = trips.length;
+        for(int i = 0; i < len; i++){
+            int[] j = trips[i];
+            far = Math.max(j[2], far);
         }
+        int[] p = new int[far + 1];
+        for(int i = 0; i < len; i++){
+            int[] k = trips[i];
+            for(int from = k[1]; from < k[2]; from++){
+                if(p[from] + k[0] <= capacity){
+                    p[from] += k[0];
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
