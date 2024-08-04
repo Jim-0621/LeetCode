ef92a9e917943306b804d8e4c0529ec362af92cb
Jim_0621 <1031352726@qq.com>
2024-08-04 18:08:55 +0800

    feat: review

---
+++ review/Solution.java
@@ -0,0 +1,41 @@
+package review;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+class Solution {
+    public String decodeString(String s) {
+        char[] sToChar = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        Deque<Character> stk = new ArrayDeque<>();
+        for (Character i : sToChar) {
+            if (i == ']') {
+                List<Character> tmp = new ArrayList<>();
+                while (stk.peek() != '[') {
+                    tmp.add(stk.pop());
+                }
+                stk.pop();
+                int count = 0;
+                int carry = 1;
+                while (!stk.isEmpty() && stk.peek() <= '9' && stk.peek() >= '0') {
+                    count += carry * (stk.pop() - '0');
+                    carry *= 10;
+                }
+                while (count > 0) {
+                    for (int j = tmp.size(); j > 0; j--) {
+                        stk.push(tmp.get(j - 1));
+                    }
+                    count--;
+                }
+            } else {
+                stk.push(i);
+            }
+        }
+        while (!stk.isEmpty()) {
+            sb.append(stk.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
---
+++ review/test.java
@@ -0,0 +1,10 @@
+package review;
+
+public class test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "3[a]2[bc]";
+        System.out.println(solution.decodeString(s));
+
+    }
+}
